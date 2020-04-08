ac2822a118fbfed84383ace6c4f190fb94bb4b76
xiaozhan <Yu.Wang@zhan.com>
2020-04-08 10:51:12 +0800

    修复

---
+++ CMUtils/Classes/XZConstant.h
@@ -0,0 +1,17 @@
+//
+//  XZConstant.h
+//  CMUtils
+//
+//  Created by 小站 on 2020/4/8.
+//
+
+#ifndef XZConstant_h
+#define XZConstant_h
+
+// 事件传递
+#import "UIResponder+RouterEvent.h"
+
+// 获取图片
+#import "UIImage+bundle.h"
+
+#endif /* XZConstant_h */
